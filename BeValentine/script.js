6c877181f94741e29b220af46226906191df0560
ronjarbh <ronjarosenbach@gmail.com>
2024-02-09 17:44:01 +0100

    update BeValentine

--- BeValentine/script.js
+++ BeValentine/script.js
@@ -25,3 +25,17 @@ function moveButton() {
     noButton.style.top = `${y}px`;
     playAudio();
 }
+
+
+const novideo = document.getElementById('novideo');
+const infoText = document.getElementById('infoText');
+
+novideo.addEventListener('play', function() {
+    if (novideo.currentTime >= 0 && novideo.currentTime <= 10) { // Ändere die Zeit entsprechend der gewünschten Dauer der traurigen Musik
+        infoText.style.display = 'block';
+    }
+});
+
+novideo.addEventListener('ended', function() {
+    infoText.style.display = 'none';
+});
